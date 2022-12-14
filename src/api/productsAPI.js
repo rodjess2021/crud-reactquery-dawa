5a00b89ce76fab46bd3ece535d4c355660b903b7
rodjess2021 <rodrigo.valer@tecsup.edu.pe>
2022-12-14 16:54:06 -0500

    first

---
+++ src/api/productsAPI.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const productsApi = axios.create({
+    baseURL: 'http://localhost:3000/products'
+})
+
+export const getProducts = async () => {
+    const res = await productsApi.get('/')
+    return res.data;
+}
+
+export const createProduct = (product) => productsApi.post('/', product)
+
+export const deleteProduct = id => productsApi.delete(`/${id}`)
+
+export const updateProduct = (product) => productsApi.put(`/${product.id}`, product)
